196ded98e47c513965a95e148a35706a06075b31
Alexis Comte <alexis.comte73@icloud.com>
2025-01-22 12:05:12 +0100

    update breakpoints for sidebar

--- src/layout/sidebar-layout.tsx
+++ src/layout/sidebar-layout.tsx
@@ -12,9 +12,9 @@ export default function SidebarLayout() {
 
   return (
     <div className="flex">
-      <div id="sidebar" className="sticky top-0 h-screen">
+      <aside id="sidebar" className="xl:sticky xl:top-0 xl:h-screen">
         <Sidebar sidebarOpen={open} toggleOpen={handleToggleSidebar} />
-      </div>
+      </aside>
       <main className="flex-1 px-4 md:px-10 h-800">
         <Outlet />
       </main>
