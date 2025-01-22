0aa2b22d9db998b7aac0442aaf514395ca5837d0
Alexis Comte <alexis.comte73@icloud.com>
2025-01-22 12:06:04 +0100

    patch main height

--- src/layout/sidebar-layout.tsx
+++ src/layout/sidebar-layout.tsx
@@ -15,7 +15,7 @@ export default function SidebarLayout() {
       <aside id="sidebar" className="xl:sticky xl:top-0 xl:h-screen">
         <Sidebar sidebarOpen={open} toggleOpen={handleToggleSidebar} />
       </aside>
-      <main className="flex-1 px-4 md:px-10 h-800">
+      <main className="flex-1 px-4 md:px-10">
         <Outlet />
       </main>
     </div>
