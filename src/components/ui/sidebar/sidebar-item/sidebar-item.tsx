84d927ac788430fb999e42f0a58a2256c267e777
Alexis Comte <alexis.comte73@icloud.com>
2025-01-22 10:04:31 +0100

    update logo in sidebar

--- src/components/ui/sidebar/sidebar-item/sidebar-item.tsx
+++ src/components/ui/sidebar/sidebar-item/sidebar-item.tsx
@@ -19,11 +19,13 @@ export default function SidebarItem({
       className={`text-grey-900 border-b-4 pt-2 pb-3 w-full rounded-t-2 xl:border-b-none  xl:border-l-4 xl:rounded-l-none xl:rounded-r-3 xl:py-4 ${active ? "bg-beige-100 border-green" : "border-transparent"} ${sidebarOpen ? "xl:px-8" : "xl:flex xl:justify-end xl:pr-6"}`}
     >
       <div
-        className={`flex flex-col items-center gap-y-1 xl:flex-row xl:items-center xl:gap-x-4 ${active ? "" : "text-grey-300 hover:text-white"}`}
+        className={`flex flex-col items-center gap-y-1 xl:flex-row xl:items-center xl:gap-x-4 xl:h-6 overflow-hidden ${active ? "" : "text-grey-300 hover:text-white"}`}
       >
-        <Icon className={`text-6 ${active ? "text-green" : ""}`} />
+        <Icon
+          className={`text-6 min-h-6 min-w-6 ${active ? "text-green" : ""}`}
+        />
         <p
-          className={`font-bold text-3 xl:text-4 line-height-150% hidden md:inline-block ${active ? "text-grey-900" : ""} ${sidebarOpen ? "xl:block" : "xl:hidden"}`}
+          className={`font-bold text-3 xl:text-4 line-height-150% hidden md:inline-block flex-1 ${active ? "text-grey-900" : ""} ${sidebarOpen ? "xl:block" : "xl:hidden"}`}
         >
           {name}
         </p>
--- src/components/ui/sidebar/sidebar.tsx
+++ src/components/ui/sidebar/sidebar.tsx
@@ -1,6 +1,7 @@
 import {
   BillsIcon,
   BrandIcon,
+  BrandIconSmall,
   BudgetIcon,
   HomeIcon,
   MinimizeIcon,
@@ -22,7 +23,11 @@ export default function Sidebar() {
       className={`flex flex-col gap-y-6 px-4 pt-2 bg-grey-900 rounded-t-2 fixed bottom-0 left-0 right-0 xl:static xl:rounded-l-none xl:rounded-r-2 xl:min-h-screen xl:pb-6 xl:transition-all xl:duration-300 ${sidebarOpen ? "xl:w-75 xl:pl-0 xl:pr-6" : "xl:w-20 xl:pl-0 xl:pr-2"}`}
     >
       <div className="hidden xl:block pl-8 py-10">
-        <BrandIcon className="text-5.5" />
+        {sidebarOpen ? (
+          <BrandIcon className="text-5.5" />
+        ) : (
+          <BrandIconSmall className="text-5.5" />
+        )}
       </div>
       <div className="flex w-full xl:flex-col xl:gap-y-1 flex-1">
         <SidebarItem
@@ -61,8 +66,10 @@ export default function Sidebar() {
           onClick={handleToggleSidebar}
           className="flex items-center gap-x-4"
         >
-          <MinimizeIcon className="text-6" />
-          <span className="line-height-150% font-bold text-4">
+          <MinimizeIcon className="text-6 xl:min-h-6 xl:min-w-6" />
+          <span
+            className={`line-height-150% font-bold text-4 h-6 ${sidebarOpen ? "xl:block" : "xl:hidden"}`}
+          >
             Minimize Menu
           </span>
         </button>
