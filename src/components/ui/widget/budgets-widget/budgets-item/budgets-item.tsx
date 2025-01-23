daef6cde46264250403d996113fc8709008d3eaf
Alexis Comte <alexis.comte73@icloud.com>
2025-01-23 10:48:39 +0100

    fix the budgets widget

--- src/components/ui/widget/budgets-widget/budgets-item/budgets-item.tsx
+++ src/components/ui/widget/budgets-widget/budgets-item/budgets-item.tsx
@@ -17,7 +17,7 @@ export default function BudgetsItem({
           : "bg-navy";
   return (
     <div className="flex w-full gap-x-4">
-      <div className={`w-1 h-full rounded-full ${borderColor}`}></div>
+      <div className={`flex-1 w-1 max-w-1 rounded-full ${borderColor}`}></div>
       <div className="flex flex-col gap-y-1">
         <p className="text-3 line-height-150% text-grey-500">{title}</p>
         <p className="text-3.5 line-height-150% text-grey-900 font-bold">
--- src/components/ui/widget/budgets-widget/budgets-widget.tsx
+++ src/components/ui/widget/budgets-widget/budgets-widget.tsx
@@ -6,24 +6,28 @@ const BUDGETS = [
   {
     name: "Entertainment",
     goal: "50",
+    current: "10",
     color: "#277C78",
     colorName: "green",
   },
   {
     name: "Bills",
     goal: "750",
+    current: "208",
     color: "#3F82B2",
     colorName: "blue",
   },
   {
     name: "Dining Out",
     goal: "75",
+    current: "70",
     color: "#F2CDAC",
     colorName: "yellow",
   },
   {
     name: "Personal Care",
     goal: "100",
+    current: "50",
     color: "#626070",
     colorName: "grey",
   },
@@ -66,16 +70,19 @@ export default function BudgetsWidget() {
   return (
     <div className="bg-white px-5 py-6 rounded-3 flex flex-col gap-y-5 md:p-8">
       <WidgetHeader title="Budgets" href="/budgets" linkName="See Details" />
-      <div className="flex flex-col gap-y-4 md:flex-row md:justify-center md:gap-x-4">
+      <div className="flex flex-col flex-1 gap-y-4 md:flex-row md:justify-center items-center md:gap-x-4">
         <div
           ref={circleRef}
-          className="w-60 h-60 max-w-60 max-h-60 rounded-full bg-red-3 gradient-circle flex items-center justify-center mx-auto xl:mx-0"
+          className="flex items-center justify-center w-full max-w-60 aspect-square rounded-full mx-auto xl:mx-0 relative"
         >
-          <div className="w-47 h-47 bg-white rounded-full opacity-25 absolute"></div>
-          <div className="w-40 h-40 bg-white rounded-full flex justify-center items-center flex-col gap-y-2">
-            <p className="font-bold line-height-120% text-8">$338</p>
+          <div className="w-68% h-68% bg-white rounded-full opacity-25 absolute"></div>
+          <div className="w-60% h-60% bg-white rounded-full flex justify-center items-center flex-col gap-y-2">
+            <p className="font-bold line-height-120% text-8">
+              ${BUDGETS.reduce((curr, acc) => curr + Number(acc.current), 0)}
+            </p>
             <p className="text-3 line-height-150% text-grey-500">
-              of $975 limit
+              of ${BUDGETS.reduce((curr, acc) => curr + Number(acc.goal), 0)}{" "}
+              limit
             </p>
           </div>
         </div>
