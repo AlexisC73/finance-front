a9f68d0a585584df040cb396262f9e6a699d29dd
Alexis Comte <alexis.comte73@icloud.com>
2025-01-23 11:19:10 +0100

    update layout for circle in budget widget

--- src/components/ui/widget/budgets-widget/budgets-widget.tsx
+++ src/components/ui/widget/budgets-widget/budgets-widget.tsx
@@ -37,8 +37,10 @@ export default function BudgetsWidget() {
   return (
     <div className="bg-white px-5 py-6 rounded-3 flex flex-col gap-y-5 md:p-8">
       <WidgetHeader title="Budgets" href="/budgets" linkName="See Details" />
-      <div className="flex flex-col flex-1 gap-y-4 md:flex-row md:justify-center items-center md:gap-x-4">
-        <BudgetCircle budgets={BUDGETS} />
+      <div className="flex flex-col flex-1 gap-y-4 md:flex-row md:justify-between items-center md:gap-x-4">
+        <div className="flex flex-1 h-60 w-60 items-center justify-center">
+          <BudgetCircle budgets={BUDGETS} />
+        </div>
         <div className="grid cols-2 w-full md:w-auto gap-4 md:flex md:flex-col">
           {BUDGETS.map((budget) => (
             <BudgetsItem
