802b9b360bb9697b65835fbdd06c26fb74832e4c
Alexis Comte <alexis.comte73@icloud.com>
2025-01-23 09:05:14 +0100

    updating layout padding

--- src/components/ui/widget/pots-widget/pots-widget.tsx
+++ src/components/ui/widget/pots-widget/pots-widget.tsx
@@ -5,7 +5,7 @@ import PotsDetailList from "./pots-detail-list/pots-detail-list";
 export function PotsWidget() {
   return (
     <div className="bg-white flex w-full flex-col py-6 px-5 gap-y-5 rounded-3">
-      <WidgetHeader title="Pots" href="/pots" linkName="View All" />
+      <WidgetHeader title="Pots" href="/pots" linkName="See Details" />
       <div className="w-full flex flex-col gap-y-5">
         <div className="w-full flex bg-beige-100 rounded-3 py-5 px-4 items-center gap-x-4">
           <PotsIconOutline className="text-10 text-green" />
--- src/layout/sidebar-layout.tsx
+++ src/layout/sidebar-layout.tsx
@@ -15,7 +15,7 @@ export default function SidebarLayout() {
       <aside id="sidebar" className="xl:sticky xl:top-0 xl:h-screen">
         <Sidebar sidebarOpen={open} toggleOpen={handleToggleSidebar} />
       </aside>
-      <main className="flex-1 px-4 md:px-10">
+      <main className="flex-1 px-4 py-6 md:py-8 md:px-10 xl:p-10">
         <Outlet />
       </main>
     </div>
