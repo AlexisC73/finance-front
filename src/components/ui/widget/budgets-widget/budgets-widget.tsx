da1da182b6a7de70daa5febc2713c5f2aac7dbb2
Alexis Comte <alexis.comte73@icloud.com>
2025-01-23 10:17:52 +0100

    adding budgets widget

---
+++ src/components/ui/widget/budgets-widget/budgets-widget.tsx
@@ -0,0 +1,95 @@
+import { useLayoutEffect, useRef } from "react";
+import { WidgetHeader } from "../widget-header/widget-header";
+import BudgetsItem from "./budgets-item/budgets-item";
+
+const BUDGETS = [
+  {
+    name: "Entertainment",
+    goal: "50",
+    color: "#277C78",
+    colorName: "green",
+  },
+  {
+    name: "Bills",
+    goal: "750",
+    color: "#3F82B2",
+    colorName: "blue",
+  },
+  {
+    name: "Dining Out",
+    goal: "75",
+    color: "#F2CDAC",
+    colorName: "yellow",
+  },
+  {
+    name: "Personal Care",
+    goal: "100",
+    color: "#626070",
+    colorName: "grey",
+  },
+];
+
+export default function BudgetsWidget() {
+  const circleRef = useRef<HTMLDivElement>(null);
+
+  const getGradient = (
+    budgets: { name: string; goal: string; color: string }[],
+  ) => {
+    const totalAmount = budgets.reduce(
+      (acc, curr) => acc + parseInt(curr.goal),
+      0,
+    );
+    const colorPercentages = budgets.map((budget) => ({
+      color: budget.color,
+      percentage: Math.round((parseInt(budget.goal) / totalAmount) * 100),
+    }));
+
+    let currentPercentage = 0;
+
+    const gradientsCSS = colorPercentages.map((colorPercentage) => {
+      const cssProperty = `${colorPercentage.color} ${currentPercentage}% ${currentPercentage + colorPercentage.percentage}%`;
+      currentPercentage += colorPercentage.percentage;
+      return cssProperty;
+    });
+
+    if (!circleRef.current) {
+      return;
+    }
+
+    circleRef.current.style.background = `conic-gradient(${gradientsCSS.join(", ")})`;
+  };
+
+  useLayoutEffect(() => {
+    getGradient(BUDGETS);
+  });
+
+  return (
+    <div className="bg-white px-5 py-6 rounded-3 flex flex-col gap-y-5">
+      <WidgetHeader title="Budgets" href="/budgets" linkName="See Details" />
+      <div className="flex flex-col gap-y-4 md:flex-row md:justify-center md:gap-x-4">
+        <div
+          ref={circleRef}
+          className="w-60 h-60 max-w-60 max-h-60 rounded-full bg-red-3 gradient-circle flex items-center justify-center mx-auto xl:mx-0"
+        >
+          <div className="w-47 h-47 bg-white rounded-full opacity-25 absolute"></div>
+          <div className="w-40 h-40 bg-white rounded-full flex justify-center items-center flex-col gap-y-2">
+            <p className="font-bold line-height-120% text-8">$338</p>
+            <p className="text-3 line-height-150% text-grey-500">
+              of $975 limit
+            </p>
+          </div>
+        </div>
+        <div className="grid cols-2 gap-4 md:flex md:flex-col">
+          {BUDGETS.map((budget) => (
+            <BudgetsItem
+              amount={budget.goal}
+              title={budget.name}
+              color={budget.colorName}
+              key={budget.name}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,25 @@
 import SectionTitle from "@/components/ui/section-title/section-title";
 import Summary from "@/components/ui/summary/summary";
+import BudgetsWidget from "@/components/ui/widget/budgets-widget/budgets-widget";
+import { PotsWidget } from "@/components/ui/widget/pots-widget/pots-widget";
+import RecurringBillsWidget from "@/components/ui/widget/recurring-bills-widget/recurring-bills-widget";
+import TransactionWidget from "@/components/ui/widget/transaction-widget/transaction-widget";
 
 export function HomePage() {
   return (
-    <div className="flex flex-col gap-y-8">
+    <div className="flex flex-col gap-y-8 pb-50">
       <SectionTitle>Overview</SectionTitle>
       <Summary />
+      <div className="flex flex-col xl:grid xl:cols-3 gap-x-6 gap-y-4">
+        <div className="flex flex-col w-full gap-y-4 xl:col-span-2">
+          <PotsWidget />
+          <TransactionWidget />
+        </div>
+        <div className="flex flex-col w-full gap-y-4 xl:col-span-1">
+          <BudgetsWidget />
+          <RecurringBillsWidget />
+        </div>
+      </div>
     </div>
   );
 }
