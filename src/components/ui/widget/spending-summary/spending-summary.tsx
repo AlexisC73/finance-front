3437a73812174bca0c9fb205245085007d91a86b
Alexis Comte <alexis.comte73@icloud.com>
2025-01-24 10:43:44 +0100

    adding responsive for spending widget

--- src/components/ui/widget/spending-summary/spending-summary.tsx
+++ src/components/ui/widget/spending-summary/spending-summary.tsx
@@ -5,9 +5,11 @@ import SpendingSummaryItem from "./spending-item/spending-item";
 
 export default function SpendingSummaryWidget() {
   return (
-    <div className="bg-white rounded-3 flex flex-col items-center py-6 px-5 gap-y-8">
-      <BudgetCircle budgets={BUDGETS} />
-      <div className="w-full flex flex-col gap-y-6">
+    <div className="bg-white rounded-3 flex flex-col items-center py-6 px-5 gap-y-8 md:flex-row md:gap-x-8 xl:flex-col md:p-8">
+      <div className="flex md:flex-1 min-w-60 justify-center items-center">
+        <BudgetCircle budgets={BUDGETS} />
+      </div>
+      <div className="w-full md:w-74 flex flex-col gap-y-6">
         <h3 className="text-5 line-height-120% font-bold">Spending Summary</h3>
         <ul className="flex flex-col gap-y-4">
           {BUDGETS.map((budget, index) => (
