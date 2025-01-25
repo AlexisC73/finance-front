9bceabadfd2a7d1a5061b54ee4fbb9c74a5b4f39
Alexis Comte <alexis.comte73@icloud.com>
2025-01-25 16:37:56 +0100

    update page selector

--- src/components/ui/transaction-view/page-selector/page-button/page-button.tsx
+++ src/components/ui/transaction-view/page-selector/page-button/page-button.tsx
@@ -1,15 +1,16 @@
 export default function PageButton({
-  text,
-  active,
+  page,
+  currentPage,
 }: {
-  text: string;
-  active: boolean;
+  page: number | "...";
+  currentPage: number;
 }) {
+  const active = currentPage === Number(page);
   return (
     <button
-      className={`w-10 h-10 rounded-2 border border-beige-500 text-preset-4 flex items-center justify-center ${active ? "bg-grey-900 text-white" : "border-beige-500"}`}
+      className={`w-10 h-10 rounded-2 border border-beige-500 text-preset-4 flex items-center justify-center hover:text-white hover:bg-beige-500 hover:bg-op-100 ${active ? "bg-grey-900 text-white" : "border-beige-500"}`}
     >
-      {text}
+      {page}
     </button>
   );
 }
--- src/components/ui/transaction-view/page-selector/page-selector.tsx
+++ src/components/ui/transaction-view/page-selector/page-selector.tsx
@@ -1,87 +1,138 @@
-import { useState } from "react";
-import NextPreviousPageButton from "./next-previous-page-button/prev-next-page-button";
 import PageButton from "./page-button/page-button";
+
+const calculateEachSidePage = ({
+  currentPage,
+  maxSidePage,
+  lastPageNumber,
+}: {
+  currentPage: number;
+  maxSidePage: number;
+  lastPageNumber: number;
+}): { left: number; right: number } => {
+  const leftAvailable = currentPage - 2;
+  const rightAvailable = lastPageNumber - currentPage - 1;
+
+  const result = { left: 0, right: 0 };
+
+  if (leftAvailable < maxSidePage && rightAvailable < maxSidePage) {
+    return { left: leftAvailable, right: rightAvailable };
+  }
+
+  if (leftAvailable < maxSidePage && rightAvailable >= maxSidePage) {
+    result.left = leftAvailable;
+    result.right = Math.min(
+      maxSidePage + (maxSidePage - leftAvailable),
+      rightAvailable,
+    );
+  }
+
+  if (leftAvailable >= maxSidePage && rightAvailable < maxSidePage) {
+    result.left = Math.min(
+      maxSidePage + (maxSidePage - rightAvailable),
+      leftAvailable,
+    );
+    result.right = rightAvailable;
+  }
+
+  if (leftAvailable >= maxSidePage && rightAvailable >= maxSidePage) {
+    result.left = maxSidePage;
+    result.right = maxSidePage;
+  }
+
+  return result;
+};
 
 export default function PageSelector({
   currentPage,
-  maxPage,
-  updatePage,
+  lastPageNumber,
+  maxSidePage,
 }: {
   currentPage: number;
-  maxPage: number;
+  lastPageNumber: number;
   updatePage: (page: number) => void;
+  maxSidePage: number;
 }) {
-  const [lastMove, setLastMove] = useState<"prev" | "next">("next");
-
-  const handlePageChange = (page: number, move: "prev" | "next") => {
-    if (page < 1 || page > maxPage) return;
-    setLastMove(move);
-    updatePage(page);
-  };
+  const pageCount = calculateEachSidePage({
+    maxSidePage: maxSidePage - 1,
+    currentPage,
+    lastPageNumber: lastPageNumber,
+  });
 
   return (
-    <div
-      id="page-list"
-      className="flex gap-x-2 pt-6 md:justify-between mx-auto md:mx-none"
-    >
-      <NextPreviousPageButton
-        action={() => handlePageChange(currentPage - 1, "prev")}
-        type="previous"
+    <div className="flex gap-x-4">
+      <PageButton page={1} currentPage={currentPage} />
+      <LeftPageRest currentPage={currentPage} pageToDisplay={pageCount.left} />
+      {currentPage > 1 && currentPage < lastPageNumber && (
+        <PageButton page={currentPage} currentPage={currentPage} />
+      )}
+      <RightPageRest
+        currentPage={currentPage}
+        maxPage={lastPageNumber}
+        pageToDisplay={pageCount.right}
       />
-      <div className="gap-x-2 md:hidden flex">
-        {maxPage > 4 ? (
-          <>
-            <PageButton active={currentPage === 1} text="1" />
-            {currentPage <= 2 && (
-              <PageButton active={currentPage === 2} text="2" />
-            )}
-            {((lastMove === "prev" && currentPage > 2) ||
-              currentPage >= maxPage - 1) && (
-              <PageButton text="..." active={false} />
-            )}
-            {currentPage > 2 && currentPage < maxPage - 1 && (
-              <PageButton text={currentPage.toString()} active={true} />
-            )}
-            {((lastMove === "next" && currentPage < maxPage - 1) ||
-              currentPage <= 2) && <PageButton text="..." active={false} />}
-            {currentPage >= maxPage - 1 && (
-              <PageButton
-                active={currentPage === maxPage - 1}
-                text={(maxPage - 1).toString()}
-              />
-            )}
-            <PageButton
-              active={currentPage === maxPage}
-              text={maxPage.toString()}
-            />
-          </>
-        ) : (
-          <>
-            {Array.from({ length: maxPage }, (_, i) => i + 1).map(
-              (page, index) => (
-                <PageButton
-                  key={page}
-                  active={currentPage === index + 1}
-                  text={(index + 1).toString()}
-                />
-              ),
-            )}
-          </>
-        )}
-      </div>
-      <div className="gap-x-2 hidden md:flex">
-        {Array.from({ length: maxPage }, (_, i) => i + 1).map((page, index) => (
-          <PageButton
-            key={page}
-            active={currentPage === index + 1}
-            text={(index + 1).toString()}
-          />
-        ))}
-      </div>
-      <NextPreviousPageButton
-        action={() => handlePageChange(currentPage + 1, "next")}
-        type="next"
-      />
+      <PageButton page={lastPageNumber} currentPage={currentPage} />
     </div>
+  );
+}
+
+function LeftPageRest({
+  currentPage,
+  pageToDisplay,
+}: {
+  currentPage: number;
+  pageToDisplay: number;
+}) {
+  const shouldDisplayDots = currentPage - 2 > pageToDisplay;
+  return (
+    <>
+      {Array.from({ length: pageToDisplay }).map((_, index) =>
+        index !== 0 ? (
+          <PageButton
+            key={index}
+            currentPage={currentPage}
+            page={currentPage - pageToDisplay + index}
+          />
+        ) : (
+          <PageButton
+            key={index}
+            currentPage={currentPage}
+            page={
+              shouldDisplayDots ? "..." : currentPage - pageToDisplay + index
+            }
+          />
+        ),
+      )}
+    </>
+  );
+}
+
+function RightPageRest({
+  currentPage,
+  pageToDisplay,
+  maxPage,
+}: {
+  currentPage: number;
+  pageToDisplay: number;
+  maxPage: number;
+}) {
+  const shouldDisplayDots = currentPage + pageToDisplay < maxPage - 1;
+  return (
+    <>
+      {Array.from({ length: pageToDisplay }).map((_, index) =>
+        index + 1 !== pageToDisplay ? (
+          <PageButton
+            key={index}
+            page={currentPage + index + 1}
+            currentPage={currentPage}
+          />
+        ) : (
+          <PageButton
+            key={index}
+            page={shouldDisplayDots ? "..." : currentPage + index + 1}
+            currentPage={currentPage}
+          />
+        ),
+      )}
+    </>
   );
 }
