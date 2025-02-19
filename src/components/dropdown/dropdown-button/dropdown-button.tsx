0ce9774c47527d260e2daaa47dd17a6eb00deeda
Alexis Comte <alexis.comte73@icloud.com>
2025-02-19 11:52:33 +0100

    fix dropdown button size

--- src/components/dropdown/dropdown-button/dropdown-button.tsx
+++ src/components/dropdown/dropdown-button/dropdown-button.tsx
@@ -11,7 +11,7 @@ export default function DropdownButton({
   return (
     <button
       onClick={action}
-      className="flex gap-x-4 text-preset-4 items-center border border-beige-500 w-full px-5 py-3 justify-between rounded-2"
+      className="flex gap-x-4 text-preset-4 items-center border border-beige-500 flex-1 px-5 py-3 justify-between rounded-2"
     >
       {children} <ArrowIcon className="rotate-90" />
     </button>
