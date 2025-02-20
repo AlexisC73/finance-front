51a0558ca04e52a31f107ad4aae8e84855819899
Alexis Comte <alexis.comte73@icloud.com>
2025-02-20 10:32:39 +0100

    update for desktop view

--- src/pages/Recurring-bills.tsx
+++ src/pages/Recurring-bills.tsx
@@ -12,40 +12,46 @@ export default function RecurringBillsPage() {
   return (
     <div className="flex flex-col gap-y-8 pb-14">
       <SectionTitle>Recurring Bills</SectionTitle>
-      <div className="flex flex-col md:grid md:cols-2 gap-y-6 md:gap-x-6">
+      <div id="bills-content" className="xl:grid xl:cols-3">
         <div
-          id="total-bills"
-          className="bg-grey-900 text-white flex gap-x-5 items-center px-5 py-6 rounded-3 md:items-start md:flex-col md:p-6 md:pt-9.5 md:gap-y-8"
+          id="bills-summary-container"
+          className="flex flex-col md:grid md:cols-2 gap-y-6 md:gap-x-6 xl:flex"
         >
-          <BillsIconOutline className="text-10" />
-          <div className="flex flex-col gap-y-2.75">
-            <p className="text-preset-4">Total Bills</p>
-            <p className="text-preset-1">$384.98</p>
+          <div
+            id="total-bills"
+            className="bg-grey-900 text-white flex gap-x-5 items-center px-5 py-6 rounded-3 md:items-start md:flex-col md:p-6 md:pt-9.5 md:gap-y-8"
+          >
+            <BillsIconOutline className="text-10" />
+            <div className="flex flex-col gap-y-2.75">
+              <p className="text-preset-4">Total Bills</p>
+              <p className="text-preset-1">$384.98</p>
+            </div>
+          </div>
+          <div
+            id="bills-summary"
+            className="bg-white flex flex-col gap-y-5 p-5 rounded-3"
+          >
+            <p className="text-preset-3">Summary</p>
+            <ul className="flex flex-col gap-y-4">
+              {BILLS.map((bill, index) => (
+                <React.Fragment key={bill.title}>
+                  <SummaryItem
+                    title={bill.title}
+                    quantity={bill.quantity}
+                    total={bill.total}
+                  />
+                  {index < BILLS.length - 1 && (
+                    <div
+                      id="separator"
+                      className="h-px bg-[#696868] bg-opacity-15 w-full"
+                    ></div>
+                  )}
+                </React.Fragment>
+              ))}
+            </ul>
           </div>
         </div>
-        <div
-          id="bills-summary"
-          className="bg-white flex flex-col gap-y-5 p-5 rounded-3"
-        >
-          <p className="text-preset-3">Summary</p>
-          <ul className="flex flex-col gap-y-4">
-            {BILLS.map((bill, index) => (
-              <React.Fragment key={bill.title}>
-                <SummaryItem
-                  title={bill.title}
-                  quantity={bill.quantity}
-                  total={bill.total}
-                />
-                {index < BILLS.length - 1 && (
-                  <div
-                    id="separator"
-                    className="h-px bg-[#696868] bg-opacity-15 w-full"
-                  ></div>
-                )}
-              </React.Fragment>
-            ))}
-          </ul>
-        </div>
+        <div id="bills-transactions"></div>
       </div>
     </div>
   );
