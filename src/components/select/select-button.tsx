ba85e017928bb813b01c258dde947b0c1c9e742d
Alexis Comte <alexis.comte73@icloud.com>
2025-02-26 16:26:52 +0100

    update select button

--- src/components/select/select-button.tsx
+++ src/components/select/select-button.tsx
@@ -1,19 +1,10 @@
 import { ArrowIcon } from "@/assets/icons/icons";
 import { ReactNode } from "react";
 
-export default function SelectButton({
-  children,
-  action,
-}: {
-  children: ReactNode;
-  action?: () => void;
-}) {
+export default function SelectButton({ children }: { children: ReactNode }) {
   return (
-    <button
-      onClick={action}
-      className="flex gap-x-4 text-preset-4 items-center border border-beige-500 flex-1 px-5 py-3 justify-between rounded-2"
-    >
+    <div className="flex gap-x-4 text-preset-4 items-center border border-beige-500 flex-1 px-5 py-3 justify-between rounded-2">
       {children} <ArrowIcon className="rotate-90" />
-    </button>
+    </div>
   );
 }
